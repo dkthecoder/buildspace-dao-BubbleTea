a2d5233b4afb42f14bfa0eff2dbbde3600dbaf25
dkthecoder <dkthecoder@coder.com>
2022-09-11 11:48:48 +0100

    changed background, setup app.jsx with connect wallet

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import { useAddress, useMetamask } from '@thirdweb-dev/react';
+
 const App = () => {
+  //third web hooks
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+  console.log("👋 Address:", address);
+
+  //no wallet connected case
+  //calls connectWallet method
+  if(!address){
+    return (
+      <div className="landing">
+        <h1>Welcome to BubbleTeaDAO</h1>
+        <button onClick={connectWithMetamask} className="btn-hero">
+          Connect your wallet
+        </button>
+      </div>
+    );
+  }
+
+  //connected wallet
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>👀 wallet connected, now what!</h1>
     </div>
   );
 };
